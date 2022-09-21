0bc8ad10401a9884b1f9cc258310f0b86baef30b
sebireni <sebireni@gmail.com>
2022-09-21 21:11:38 +0200

    single_double_quotes_uniformized

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 let rawData = fs.readFileSync("./data.json");
 let data = JSON.parse(rawData);
 
@@ -9,20 +9,21 @@ const server = express();
 
 server.use(express.json());
 
-server.use("/static", express.static(path.join("../", "frontend", "build", "static")));
+server.use(
+  "/static",
+  express.static(path.join("../", "frontend", "build", "static"))
+);
 
 server.use(cors());
 
-server.get('/', (req, res) => {
-    res.sendFile( 
-        path.join(__dirname, '../frontend/build/index.html')
-    )
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
-server.post('/registrations', (req, res) => {
-    console.log(req.body);
-    data.push(req.body);
-    fs.writeFileSync("./data.json", JSON.stringify(data));
+server.post("/registrations", (req, res) => {
+  console.log(req.body);
+  data.push(req.body);
+  fs.writeFileSync("./data.json", JSON.stringify(data));
 });
 
 const port = 6789;
