5470c01839b3f93fd5544d2e7310aff495457dab
sebireni <sebireni@gmail.com>
2022-09-21 11:42:23 +0200

    setLoading_fixed

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,124 +1,133 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useState, useEffect } from 'react';
+import React from "react";
+import styled from "styled-components";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const registration = {firstName, lastName, email};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const registration = { firstName, lastName, email };
 
-        fetch("http://localhost:6789/registrations", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(registration)
-        }).then(() => {
-            console.log(JSON.stringify(registration)); //??
-        });
-    };
+    fetch("http://localhost:6789/registrations", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(registration),
+    }).then(() => {
+      console.log(JSON.stringify(registration)); //??
+    });
+  };
 
-    return (
-        <MainContainer className="home-main-container">
-            <div className="contanier">
-                <h1 className="display-5 text-uppercase py-5 text-center">
-                    Welcome to the home page
-                </h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit iste quidem eos dolores, a enim? Numquam nihil nisi repellat placeat! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit iste quidem eos dolores, a enim? Numquam nihil nisi repellat placeat! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit iste quidem eos dolores, a enim? Numquam nihil nisi repellat placeat! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit iste quidem eos dolores, a enim? Numquam nihil nisi repellat placeat!</p>
-                <div className="form-box">
-                    <h3 className="display-6 text-uppercase text-center py-3">
-                        registration
-                    </h3>
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-row mr-auto">
-                            <div className="col-md-6 mb-4">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="First Name"
-                                    required
-                                    value={firstName}
-                                    onChange={(e) => setFirstName(e.target.value)}
-                                />
-                            </div>
-                            <div className="col-md-6 mb-4">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Last Name"
-                                    required
-                                    value={lastName}
-                                    onChange={(e) => setLastName(e.target.value)}
-                                />
-                            </div>
-                            <div className="col-md-6 mb-4">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Email"
-                                    required
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </div>
-                        </div>
-                    <button className="btn btn-primary text-uppercase">Submit</button>
-                    </form>
-                </div>
+  return (
+    <MainContainer className="home-main-container">
+      <div className="contanier">
+        <h1 className="display-5 text-uppercase py-5 text-center">
+          Welcome to the home page
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit iste
+          quidem eos dolores, a enim? Numquam nihil nisi repellat placeat! Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Velit iste quidem
+          eos dolores, a enim? Numquam nihil nisi repellat placeat! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Velit iste quidem eos
+          dolores, a enim? Numquam nihil nisi repellat placeat! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Velit iste quidem eos
+          dolores, a enim? Numquam nihil nisi repellat placeat!
+        </p>
+        <div className="form-box">
+          <h3 className="display-6 text-uppercase text-center py-3">
+            registration
+          </h3>
+          <form onSubmit={handleSubmit}>
+            <div className="form-row mr-auto">
+              <div className="col-md-6 mb-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="First Name"
+                  required
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </div>
+              <div className="col-md-6 mb-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Last Name"
+                  required
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
+              <div className="col-md-6 mb-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
             </div>
-        </MainContainer>
-    )
-}
+            <button className="btn btn-primary text-uppercase">Submit</button>
+          </form>
+        </div>
+      </div>
+    </MainContainer>
+  );
+};
 
-export default Home
+export default Home;
 
 // STYLED COMPONENTS STYLES
 
 const MainContainer = styled.div`
-    h1 {
-        color: var(--dark-tomato);
-    }
+  h1 {
+    color: var(--dark-tomato);
+  }
 
-    p {
-        color: var(--orange);
-        padding-left: 3rem;
-        padding-right: 3rem;
-    }
+  p {
+    color: var(--orange);
+    padding-left: 3rem;
+    padding-right: 3rem;
+  }
 
-    h3 {
-        color: var(--tomato);
-    }
+  h3 {
+    color: var(--tomato);
+  }
 
-    .form-box {
-        padding: 3rem 0;
-    }
+  .form-box {
+    padding: 3rem 0;
+  }
 
-    form {
-        width: 50rem;
-        margin: 0 auto;
-        padding-bottom: 50px;
-    }
+  form {
+    width: 50rem;
+    margin: 0 auto;
+    padding-bottom: 50px;
+  }
 
-    button {
-        width: 100%;
-        background: transparent;
-        border-color: var(--dark-red);
-        &:hover {
-            background: var(--dark-tomato);
-            border-color: var(--dark-red);
-        }
-        &:focus {
-            background: var(--tomato);
-            border-color: var(--tomato);
-        }
+  button {
+    width: 100%;
+    background: transparent;
+    border-color: var(--dark-red);
+    &:hover {
+      background: var(--dark-tomato);
+      border-color: var(--dark-red);
     }
+    &:focus {
+      background: var(--tomato);
+      border-color: var(--tomato);
+    }
+  }
 
-    input {
-        background: var(--dark-red);
-        opacity: 0.7;
-        margin-left: auto;
-        margin-right: auto;
-    }
+  input {
+    background: var(--dark-red);
+    opacity: 0.7;
+    margin-left: auto;
+    margin-right: auto;
+  }
 `;
